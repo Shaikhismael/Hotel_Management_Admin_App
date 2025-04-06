bf6f746a15e7c55196704114b51bf9463dc12c58
Shaikhismael <ismail322546@gmail.com>
2025-04-06 16:05:20 +0530

    - add changes for uploader, query devtools

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,12 +8,13 @@ const StyledAppLayout = styled.div`
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
+  overflow: auto;
 `;
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
-  overflow: scroll;
+  overflow: auto;
 `;
 
 const Container = styled.div`
